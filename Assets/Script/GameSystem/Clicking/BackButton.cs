62a2ad840683791101e672ea6ddd580fc12de871
Jelly1314 <106652400+Jelly1314@users.noreply.github.com>
2024-08-09 18:10:56 +0700

    menu

---
+++ Assets/Script/GameSystem/Clicking/BackButton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackButton : MonoBehaviour
+{
+    public void OnBackButtonPressed()
+    {
+        StartCoroutine(DelayedAction());
+    }
+
+    private IEnumerator DelayedAction()
+    {
+        // Wait for 8 seconds
+        yield return new WaitForSeconds(0.0f);
+        
+        // Perform the action after the delay
+        Debug.Log("Action executed after delay!");
+        
+        // Load the next scene (replace "Scene 1" with your scene's name)
+        SceneManager.LoadScene("MainMenu");
+    }
+}
