672b963dcc793c8a618001ed2fa80651600015a4
Jelly1314 <106652400+Jelly1314@users.noreply.github.com>
2024-08-09 16:41:42 +0700

    About resumegame

--- Assets/Script/GameSystem/Clicking/PauseResumeButton.cs
+++ Assets/Script/GameSystem/Clicking/PauseResumeButton.cs
@@ -6,10 +6,7 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     public GameObject player; // Assign your player GameObject in the Inspector
-    public Camera mainCamera; // Assign your main camera in the Inspector
     private Vector3 playerPosition;
-    private Vector3 cameraPosition;
-    private Quaternion cameraRotation;
 
     void Start()
     {
@@ -20,32 +17,12 @@ public class PauseMenu : MonoBehaviour
             PlayerPrefs.GetFloat("PlayerPosZ")
         );
 
-        // Load the saved camera position and rotation from PlayerPrefs
-        cameraPosition = new Vector3(
-            PlayerPrefs.GetFloat("CameraPosX"),
-            PlayerPrefs.GetFloat("CameraPosY"),
-            PlayerPrefs.GetFloat("CameraPosZ")
-        );
-
-        cameraRotation = Quaternion.Euler(
-             PlayerPrefs.GetFloat("CameraRotX"),
-             PlayerPrefs.GetFloat("CameraRotY"),
-             PlayerPrefs.GetFloat("CameraRotZ")
-         );
-
         // Set the player's position to the saved position
         player.transform.position = playerPosition;
-
-        // Set the camera's position and rotation to the saved values
-        mainCamera.transform.position = cameraPosition;
-        mainCamera.transform.rotation = cameraRotation;
 
         // Re-enable player movement and camera control when resuming
         if (player.GetComponent<FPSController>() != null)
             player.GetComponent<FPSController>().enabled = true;
-
-        if (mainCamera.GetComponent<MouseLook>() != null)
-            mainCamera.GetComponent<MouseLook>().enabled = true;
     }
 
     public void ResumeGame()
--- Assets/Script/GameSystem/game_pause.cs
+++ Assets/Script/GameSystem/game_pause.cs
@@ -7,10 +7,6 @@ public class Game_pause : MonoBehaviour
 {
     public GameObject player; // Assign your player GameObject in the Inspector
     private Vector3 playerPosition;
-    public Camera mainCamera; // Assign your main camera in the Inspector
-    private Vector3 cameraPosition;
-    private Quaternion cameraRotation;
-
 
     void Update()
     {
@@ -26,32 +22,14 @@ public class Game_pause : MonoBehaviour
         // Store the player's current position
         playerPosition = player.transform.position;
 
-        // Store the camera's current position and rotation
-        cameraPosition = mainCamera.transform.position;
-        cameraRotation = mainCamera.transform.rotation;
-
         // Save player's position in PlayerPrefs
         PlayerPrefs.SetFloat("PlayerPosX", playerPosition.x);
         PlayerPrefs.SetFloat("PlayerPosY", playerPosition.y);
         PlayerPrefs.SetFloat("PlayerPosZ", playerPosition.z);
 
-        // Save camera's position and rotation in PlayerPrefs
-        PlayerPrefs.SetFloat("CameraPosX", cameraPosition.x);
-        PlayerPrefs.SetFloat("CameraPosY", cameraPosition.y);
-        PlayerPrefs.SetFloat("CameraPosZ", cameraPosition.z);
-        PlayerPrefs.SetFloat("CameraRotX", cameraRotation.eulerAngles.x);
-        PlayerPrefs.SetFloat("CameraRotY", cameraRotation.eulerAngles.y);
-        PlayerPrefs.SetFloat("CameraRotZ", cameraRotation.eulerAngles.z);
-        PlayerPrefs.Save();
-
         // Disable player movement and camera control
         if (player.GetComponent<FPSController>() != null)
             player.GetComponent<FPSController>().enabled = false;
-
-        if (mainCamera.GetComponent<MouseLook>() != null)
-            mainCamera.GetComponent<MouseLook>().enabled = false;
-
-
         // Load the PauseGame scene
         SceneManager.LoadScene("PauseMenu");
         UnlockCursor();
